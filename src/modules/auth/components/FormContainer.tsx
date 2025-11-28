1d30da47430a8864771a79e8727bd455dd5668e3
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2025-11-28 15:56:47 -0600

    Enhance FormContainer component to support dynamic authentication type and improve user experience
    - Added `typeAuth` prop to `FormContainer` for flexible handling of login and registration states.
    - Updated `SocialButton` to utilize the `typeAuth` prop for dynamic rendering.
    - Introduced `FieldDescription` with a conditional link for account management based on authentication type.

--- src/modules/auth/components/FormContainer.tsx
+++ src/modules/auth/components/FormContainer.tsx
@@ -1,18 +1,26 @@
-import { Field, FieldGroup, FieldSeparator } from '@/src/components/ui/field'
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldSeparator,
+} from '@/src/components/ui/field'
 import { Button } from '@/src/components/ui/button'
 import { Spinner } from '@/src/components/ui/spinner'
 import SocialButton from './ui/social-button'
+import { TypeAuth } from '../types/auth'
+import Link from 'next/link'
 
 interface Props {
   children: React.ReactNode
   loading: boolean
+  typeAuth: TypeAuth
 }
 
-export default function FormContainer({ children, loading }: Props) {
+export default function FormContainer({ children, loading, typeAuth }: Props) {
   return (
-    <FieldGroup>
+    <FieldGroup className="gap-6">
       <Field>
-        <SocialButton type="login" />
+        <SocialButton type={typeAuth} />
       </Field>
 
       <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
@@ -21,7 +29,7 @@ export default function FormContainer({ children, loading }: Props) {
 
       {children}
 
-      <Field>
+      <Field className="gap-2.5">
         <Button type="submit" className="btn-warm-red cursor-pointer">
           {loading ? (
             <>
@@ -32,6 +40,13 @@ export default function FormContainer({ children, loading }: Props) {
             'Iniciar sesión'
           )}
         </Button>
+
+        <FieldDescription className="text-center">
+          ¿{typeAuth === 'login' ? 'No tienes' : 'Ya tienes'} una cuenta?{' '}
+          <Link href={typeAuth === 'login' ? '/register' : '/login'}>
+            {typeAuth === 'login' ? 'Regístrate' : 'Inicia sesión'}
+          </Link>
+        </FieldDescription>
       </Field>
     </FieldGroup>
   )
