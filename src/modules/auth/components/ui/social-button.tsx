5700b390cd9372e51e34a6db9cb152e923656769
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2025-12-01 19:10:19 -0600

    Integrate Google sign-in functionality into SocialButton component
    - Updated the SocialButton component to utilize the useAuth hook for Google sign-in.
    - Added onClick handler to trigger signInWithGoogle, enhancing user authentication experience.

--- src/modules/auth/components/ui/social-button.tsx
+++ src/modules/auth/components/ui/social-button.tsx
@@ -1,11 +1,19 @@
 import { TypeAuth } from '../../types/auth'
+import { useAuth } from '../../hooks/useAuth'
 
 import { Button } from '@/src/components/ui/button'
 import { GoogleIcon } from './google-icon'
 
 export default function SocialButton({ type = 'login' }: { type: TypeAuth }) {
+  const { signInWithGoogle } = useAuth()
+
   return (
-    <Button variant="secondary" className="cursor-pointer">
+    <Button
+      variant="secondary"
+      type="button"
+      className="cursor-pointer"
+      onClick={signInWithGoogle}
+    >
       <GoogleIcon />
       {type === 'login' ? 'Iniciar sesión' : 'Registrarse'} con Google
     </Button>
