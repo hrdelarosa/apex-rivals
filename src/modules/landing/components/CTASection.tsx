f66005807c2ed2d87bce756e2ec996b4b5e3c95b
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2026-02-28 18:26:13 -0600

    feat: mejorar sección CTA y Hero con nuevos estilos y componentes

--- src/modules/landing/components/CTASection.tsx
+++ src/modules/landing/components/CTASection.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import TitleSection from './ui/TitleSection'
 import { Button } from '@/src/components/ui/button'
 
 export default function CTASection() {
@@ -10,22 +11,26 @@ export default function CTASection() {
           src="/cta-image-7.jpg"
           alt="Formula 1 show car in metallic red with Pirelli P Zero tires."
           className="w-full h-111 object-cover opacity-40 blur-[2px] mask-alpha mask-b-from-5%"
+          style={{
+            maskImage:
+              'linear-gradient(to bottom, black 40%, transparent 100%)',
+            WebkitMaskImage:
+              'linear-gradient(to bottom, black 40%, transparent 100%)',
+          }}
           width={1200}
           height={600}
         />
       </div>
 
-      <div className="h-full flex flex-col items-center justify-center max-w-3xl mx-auto text-center">
-        <h2 className="text-5xl font-extrabold font-exo2 mb-4">
-          Únete a la parrilla ahora mismo
-        </h2>
+      <div className="h-full flex flex-col items-center justify-center max-w-3xl mx-auto text-center px-4 lg:px-0">
+        <TitleSection title="Únete a la parrilla ahora mismo" />
 
-        <p className="text-lg text-gray-300 mb-6">
+        <p className="text-base md:text-lg text-gray-300 mb-6">
           Únete a miles de aficionados a la F1 que compiten en Apex Rivals.
           ¡Forma tu equipo antes de la próxima carrera!
         </p>
 
-        <div className="flex items-center gap-4">
+        <div className="flex flex-col sm:flex-row items-center gap-3 sm:gap-4">
           <Button variant="default" size="lg" asChild>
             <Link href="/register">Empieza a correr ahora</Link>
           </Button>
--- src/modules/landing/components/Hero.tsx
+++ src/modules/landing/components/Hero.tsx
@@ -8,10 +8,13 @@ import { Button } from '@/src/components/ui/button'
 
 export default function Hero() {
   return (
-    <section className="min-h-[calc(100vh-80px)] relative px-10 pt-8">
-      <div className="h-full w-full px-10 pt-8 overflow-hidden -z-10 absolute top-0 left-0">
+    <section
+      className="min-h-[calc(100vh-80px)] relative sm:px-4 lg:px-10 sm:pt-4 lg:pt-8 mb-24"
+      data-section="hero"
+    >
+      <div className="h-full w-full sm:px-4 lg:px-10 sm:pt-4 lg:pt-8 overflow-hidden -z-10 absolute top-0 left-0">
         <Image
-          className="w-full h-full object-cover rounded-lg mask-l-from-70% mask-b-from-60% opacity-70"
+          className="w-full h-full object-cover rounded-lg sm:mask-l-from-70% mask-b-from-45% sm:mask-b-from-60% opacity-55 sm:opacity-70"
           src="/hero-image.webp"
           alt="Formula 1 single-seater driven by Max Verstappen competing on the Singapore nighttime street circuit under artificial lights"
           width={1000}
@@ -19,33 +22,33 @@ export default function Hero() {
         />
       </div>
 
-      <div className="max-w-7xl mx-auto h-[calc(100vh-112px)] flex items-center">
+      <div className="max-w-7xl mx-auto h-[calc(100vh-112px)] px-6 lg:px-10 2xl:px-0 flex items-center">
         <div className="max-w-2xl">
           <Badge
-            variant="default"
-            className="bg-red-50 text-red-700 dark:bg-red-950 dark:text-red-300 mb-6"
+            variant="outline"
+            className="bg-red-50 text-red-700 dark:bg-red-950/70 dark:text-warm-red mb-6"
           >
             <IconPointFilled data-icon="inline-start" />
             La nueva temporada 2026 ya está aquí!
           </Badge>
 
-          <h2 className="text-7xl font-bold font-exo2 mb-4">
+          <h2 className="text-5xl sm:text-7xl sm:text-left font-bold font-exo2 mb-4">
             DOMINA LA <span className="text-warm-red">PARRILLA</span>
           </h2>
 
-          <p className="text-lg text-gray-300 mb-6">
+          <p className="text-base sm:text-lg sm:text-left text-gray-300 mb-6">
             Únete a la comunidad de Fantasy F1 más vibrante. Crea ligas privadas
             con tus amigos, compite en tiempo real y demuestra quién es el
             verdadero experto en la parrilla.
           </p>
 
-          <div className="flex items-center gap-3">
+          <div className="flex flex-row items-center gap-3">
             <Button variant="default" size="lg" className="group gap-1" asChild>
               <Link href="/register">
                 Comenzar ahora
                 <ArrowRightIcon
                   data-icon="inline-end"
-                  className="group-hover:translate-x-0.5 transition-transform"
+                  className="group-hover:translate-x-0.5 transition-transform hidden sm:inline-block"
                 />
               </Link>
             </Button>
