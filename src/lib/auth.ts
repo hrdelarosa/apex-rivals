c6833ba72ef2ac4d887b92f551c729d01ad8e77a
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2026-03-04 23:00:09 -0600

    feat: agregar configuración de autenticación con Better Auth y adaptador Drizzle

---
+++ src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { db } from '../config/db'
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+  }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: true,
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    sendOnSignIn: true,
+    autoSignInAfterVerification: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
+})
