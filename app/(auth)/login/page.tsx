56c79f66919e21f25a0ed5e632bf2ca4a942e830
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2026-03-08 01:00:39 -0600

    feat: simplificar el componente de inicio de sesión eliminando el diálogo de reenvío de verificación

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,3 @@
-import { Button } from '@/src/components/ui/button'
-import { Dialog, DialogTrigger } from '@/src/components/ui/dialog'
 import {
   Card,
   CardContent,
@@ -8,39 +6,19 @@ import {
   CardTitle,
 } from '@/src/components/ui/card'
 import FormLogin from '@/src/modules/auth/components/FormLogin'
-import ResendVerificationDialog from '@/src/modules/auth/components/ResendVerificationDialog'
 
 export default function Page() {
   return (
-    <>
-      <Card>
-        <CardHeader className="text-center">
-          <CardTitle className="text-xl">Bienvenido de nuevo</CardTitle>
+    <Card>
+      <CardHeader className="text-center">
+        <CardTitle className="text-xl">Bienvenido de nuevo</CardTitle>
 
-          <CardDescription>
-            Inicia sesión con su cuenta de Google
-          </CardDescription>
-        </CardHeader>
+        <CardDescription>Inicia sesión con su cuenta de Google</CardDescription>
+      </CardHeader>
 
-        <CardContent>
-          <FormLogin />
-        </CardContent>
-      </Card>
-
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button
-            variant="link"
-            type="button"
-            size="sm"
-            className="text-muted-foreground"
-          >
-            ¿No has recibido el correo de verificación?
-          </Button>
-        </DialogTrigger>
-
-        <ResendVerificationDialog />
-      </Dialog>
-    </>
+      <CardContent>
+        <FormLogin />
+      </CardContent>
+    </Card>
   )
 }
