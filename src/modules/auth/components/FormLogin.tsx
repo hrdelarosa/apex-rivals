e00b674f6601a1ba7f4fe317702fdf8582e49c0d
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2026-03-08 01:05:37 -0600

    feat: refactor FormLogin para integrar el diálogo de restablecimiento de contraseña y simplificar el código

--- src/modules/auth/components/FormLogin.tsx
+++ src/modules/auth/components/FormLogin.tsx
@@ -1,21 +1,11 @@
 'use client'
 
-import { MailIcon } from 'lucide-react'
 import { GoogleIcon } from '@/src/components/icons/googleIcon'
 
+import { useState } from 'react'
 import Link from 'next/link'
 import { Button } from '@/src/components/ui/button'
 import { Input } from '@/src/components/ui/input'
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/src/components/ui/dialog'
 import {
   Field,
   FieldDescription,
@@ -25,10 +15,11 @@ import {
   FieldSeparator,
 } from '@/src/components/ui/field'
 import PasswordInput from '@/src/components/PasswordInput'
+import RequestPasswordResetDialog from './RequestPasswordResetDilog'
 
-import { LoginFormTypes, loginSchema } from '../schemas/auth.schemas'
 import { useAuth } from '../hooks/useAuth'
 import { useValidatedForm } from '../hooks/useValidatedForm'
+import { LoginFormTypes, loginSchema } from '../schemas/auth.schemas'
 
 export default function FormLogin() {
   const { signIn, signInWithGoogle, loading } = useAuth()
@@ -38,111 +29,81 @@ export default function FormLogin() {
       await signIn({ email, password })
     },
   })
+  const [openDialog, setOpenDialog] = useState(false)
 
   return (
-    <form onSubmit={handleSubmit}>
-      <FieldGroup>
-        <Field>
-          <Button
-            variant="outline"
-            type="button"
-            onClick={signInWithGoogle}
-            disabled={loading}
-          >
-            <GoogleIcon />
-            Iniciar sesión con Google
-          </Button>
-        </Field>
+    <>
+      <RequestPasswordResetDialog
+        open={openDialog}
+        onOpenChange={setOpenDialog}
+      />
 
-        <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
-          O continua con
-        </FieldSeparator>
-
-        <Field>
-          <FieldGroup className="gap-3">
-            <Field>
-              <FieldLabel htmlFor="email">Correo electrónico</FieldLabel>
-              <Input
-                {...register('email')}
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                autoComplete="email"
-                autoFocus
-              />
-              <FieldError>{errors.email?.message}</FieldError>
-            </Field>
-
-            <PasswordInput
-              {...register('password')}
-              id="password"
-              error={errors.password?.message}
-              autoComplete="current-password"
+      <form onSubmit={handleSubmit}>
+        <FieldGroup>
+          <Field>
+            <Button
+              variant="outline"
+              type="button"
+              onClick={signInWithGoogle}
+              disabled={loading}
             >
-              <div className="flex items-center justify-between">
-                <FieldLabel htmlFor="password">Contraseña</FieldLabel>
-                <Dialog>
-                  <DialogTrigger asChild>
-                    <Button
-                      variant="link"
-                      type="button"
-                      size="sm"
-                      className="text-muted-foreground"
-                    >
-                      ¿Olvidaste tu contraseña?
-                    </Button>
-                  </DialogTrigger>
+              <GoogleIcon />
+              Iniciar sesión con Google
+            </Button>
+          </Field>
 
-                  <DialogContent className="sm:max-w-md">
-                    <form className="flex flex-col gap-4">
-                      <DialogHeader className="items-center">
-                        <div className="size-12 flex items-center justify-center rounded-full bg-primary/10 text-primary transition-colors hover:bg-primary/20">
-                          <MailIcon />
-                        </div>
-                        <DialogTitle>Restablecer contraseña</DialogTitle>
-                        <DialogDescription className="text-center">
-                          Ingrese su correo electrónico asociado a su cuenta y
-                          le enviaremos un enlace para restablecer su
-                          contraseña.
-                        </DialogDescription>
-                      </DialogHeader>
+          <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+            O continua con
+          </FieldSeparator>
 
-                      <Field>
-                        <FieldLabel htmlFor="email">
-                          Correo electrónico
-                        </FieldLabel>
-                        <Input
-                          id="email"
-                          type="email"
-                          placeholder="m@example.com"
-                        />
-                      </Field>
+          <Field>
+            <FieldGroup className="gap-3">
+              <Field>
+                <FieldLabel htmlFor="email">Correo electrónico</FieldLabel>
+                <Input
+                  {...register('email')}
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <FieldError>{errors.email?.message}</FieldError>
+              </Field>
 
-                      <DialogFooter>
-                        <DialogClose asChild>
-                          <Button variant="outline">Cancelar</Button>
-                        </DialogClose>
+              <PasswordInput
+                {...register('password')}
+                id="password"
+                error={errors.password?.message}
+                autoComplete="current-password"
+              >
+                <div className="flex items-center justify-between">
+                  <FieldLabel htmlFor="password">Contraseña</FieldLabel>
+                  <Button
+                    variant="link"
+                    type="button"
+                    size="sm"
+                    className="text-muted-foreground"
+                    onClick={() => setOpenDialog(true)}
+                  >
+                    ¿Olvidaste tu contraseña?
+                  </Button>
+                </div>
+              </PasswordInput>
+            </FieldGroup>
+          </Field>
 
-                        <Button type="submit">Enviar</Button>
-                      </DialogFooter>
-                    </form>
-                  </DialogContent>
-                </Dialog>
-              </div>
-            </PasswordInput>
-          </FieldGroup>
-        </Field>
+          <Field>
+            <Button type="submit" disabled={loading}>
+              Iniciar sesión
+            </Button>
 
-        <Field>
-          <Button type="submit" disabled={loading}>
-            Iniciar sesión
-          </Button>
-
-          <FieldDescription className="text-center [&>a:hover]:text-foreground">
-            ¿No tienes una cuenta? <Link href="/register">Regístrate</Link>
-          </FieldDescription>
-        </Field>
-      </FieldGroup>
-    </form>
+            <FieldDescription className="text-center [&>a:hover]:text-foreground">
+              ¿No tienes una cuenta? <Link href="/register">Regístrate</Link>
+            </FieldDescription>
+          </Field>
+        </FieldGroup>
+      </form>
+    </>
   )
 }
