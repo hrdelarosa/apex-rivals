b52f53d583071abaeb0160a1e189aa7679a6b9a1
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2026-03-15 00:27:01 -0600

    feat: agregar soporte para valores predeterminados en el hook useValidatedForm

--- src/modules/auth/hooks/useValidatedForm.ts
+++ src/modules/auth/hooks/useValidatedForm.ts
@@ -1,14 +1,21 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import {
+  FieldValues,
+  SubmitHandler,
+  useForm,
+  DefaultValues,
+} from 'react-hook-form'
 import { ZodType } from 'zod'
 
 interface Props<T extends FieldValues> {
   formSchema: ZodType<T, T>
+  defaultValues?: DefaultValues<T>
   onSubmit: SubmitHandler<T>
 }
 
 export function useValidatedForm<T extends FieldValues>({
   formSchema,
+  defaultValues,
   onSubmit,
 }: Props<T>) {
   const {
@@ -18,6 +25,7 @@ export function useValidatedForm<T extends FieldValues>({
     reset,
   } = useForm<T>({
     resolver: zodResolver(formSchema),
+    defaultValues,
   })
 
   const handleFormSubmit: SubmitHandler<T> = async (data) => {
