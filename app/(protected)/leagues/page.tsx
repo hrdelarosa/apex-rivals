73d037da0a96c6f3fc5dd12c7d752d9623cc96cb
Hozmel Roberto De la rosa Rendón <hozdelarosar@gmail.com>
2026-05-16 23:33:16 -0600

    feat: agregar página de Mis Ligas con diseño responsivo

---
+++ app/(protected)/leagues/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page() {
+  return (
+    <div className="flex flex-1 flex-col gap-4 md:gap-6 p-4 md:p-6">
+      <div>
+        <h1 className="text-4xl font-bold font-exo2">Mis Ligas</h1>
+      </div>
+    </div>
+  )
+}
